65111732240a6b77ce94361aaa0191dc0c4c312b
Brian Kaewell <briankaewell@gmail.com>
2025-10-30 17:40:26 -0400

    Refactor codebase to use update_dns (instead of network_autopilot); Create lightweight Docker image and container

---
+++ src/update_dns/agent.py
@@ -0,0 +1,34 @@
+from .watchdog import check_internet, reset_smart_plug
+from .cloudflare import sync_dns
+# from .sheets import log_to_sheets
+#from .db import log_metrics
+
+def get_public_ip():
+    return True
+
+def run_cycle():
+    # Check internet
+    internet_ok = check_internet()
+
+    # Reset smart plug if internet is down
+    if not internet_ok:
+        reset_smart_plug()
+
+    # Get public IP address and sync DNS
+    current_ip = get_public_ip()
+    dns_changed = sync_dns(current_ip)
+
+    print("Internet OK . . . . . . . . . .✅")
+
+
+
+    # # Update Google Sheets
+    # log_to_sheets(ip=current_ip,
+    #               internet_ok=internet_ok,
+    #               dns_changed=dns_changed)
+
+    # # Optional: log metrics to SQLite
+    # log_metrics(ip=current_ip,
+    #             internet_ok=internet_ok,
+    #             dns_changed=dns_changed)
+
